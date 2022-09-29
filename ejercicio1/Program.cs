8412c4da1c2d86f7d57c246cb7d18bc0f93093e5
juancueto <juan.cueto@outlook.com>
2022-09-29 18:07:29 -0500

    Ejercicios tema 3

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿using ejercicio1;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+Ejercicio1 obj = new Ejercicio1();
+int resultado = obj.Sumar(1, 2, 3);
+Console.WriteLine(resultado);
+
+Coche miCoche = new Coche();
+miCoche.AgregarPuertas(1);
+Console.WriteLine(miCoche.NumeroPuertas);
+
+Console.ReadLine();
---
+++ ejercicio1/ejercicio1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace ejercicio1
+{
+    public class Ejercicio1
+    {
+        public Ejercicio1()
+        {
+        }
+
+        public int Sumar(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+    }
+
+    public class Coche
+    {
+        private int _NumeroPuertas;
+
+        public int NumeroPuertas
+        {
+            get { return this._NumeroPuertas; }
+        }
+        public void AgregarPuertas(int puertas)
+        {
+            this._NumeroPuertas += puertas;
+        }
+    }
+}
+
